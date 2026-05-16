65c2eff3a5c3dc3a0b651ac61369502a3c17ecea
baraa12345-hub <alsawahb@gmail.com>
2026-05-16 21:00:11 +0300

    implement Largest Rectangle in Histogram algorithm

---
+++ src/Problem-6-LargestRectangle/LargestRectangle.ts
@@ -0,0 +1,20 @@
+const largestRectangle = (heights: number[]): number => {
+  let maxArea = 0;
+
+  for (let i = 0; i < heights.length; i++) {
+    let minHeight = Infinity;
+
+    for (let j = i; j < heights.length; j++) {
+      minHeight = Math.min(minHeight, heights[j]!);
+
+      const width = j - i + 1;
+      const area = minHeight * width;
+
+      maxArea = Math.max(maxArea, area);
+    }
+  }
+
+  return maxArea;
+};
+
+console.log(largestRectangle([6, 2, 5, 4, 5, 1, 6])); 
